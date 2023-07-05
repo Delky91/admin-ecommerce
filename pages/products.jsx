e647e1bcb7dfac76da5778307bd3dbef7a8cdb2a
Luis <l.mino.bustos@gmail.com>
2023-07-04 20:02:22 -0400

    fixed styles in product pages

--- pages/products.jsx
+++ pages/products.jsx
@@ -20,69 +20,72 @@ export default function Products() {
 
 	return (
 		<Layout>
-			<h2 className='flex justify-center text-white font-bold md:justify-start md:ml-2'>
-				Products
-			</h2>
-			<table className='basic my-5'>
-				<thead>
-					<tr>
-						<td className='py-2 pl-4'>Product name</td>
-						<td>Options</td>
-					</tr>
-				</thead>
-				<tbody>
-					{products.map((product) => (
-						<tr
-							key={product._id}
-							className=''>
-							<td className='text-white pl-4'>{product.title}</td>
-							<td className='flex gap-1 border-l border-gray-200/50 py-1'>
-								<Link
-									href={"/products/edit/" + product._id}
-									className='px-4 border hover:bg-primary hover:shadow-sm hover:shadow-white'>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										fill='none'
-										viewBox='0 0 24 24'
-										strokeWidth={1.5}
-										stroke='currentColor'
-										className='w-4 h-4'>
-										<path
-											strokeLinecap='round'
-											strokeLinejoin='round'
-											d='M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10'
-										/>
-									</svg>
-									Edit
-								</Link>
-								<button
-									className='btn-accent flex items-center shadow-sm hover:text-accent hover:bg-white hover:shadow-accent'
-									onClick={() => deleteConfirmation(product, page, fetchProduct)}>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										fill='none'
-										viewBox='0 0 24 24'
-										strokeWidth={1.5}
-										stroke='currentColor'
-										className='w-4 h-4'>
-										<path
-											strokeLinecap='round'
-											strokeLinejoin='round'
-											d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
-										/>
-									</svg>
-									Delete
-								</button>
-							</td>
+			<h2 className='text-white font-bold md:ml-2 text-center'>Products</h2>
+			<div className=' rounded-md my-5 md:w-2/3 mx-auto'>
+				<table className='basic rounded-lg'>
+					<thead>
+						<tr>
+							<td className='py-2 pl-4'>Product name</td>
+							<td>Options</td>
 						</tr>
-					))}
-				</tbody>
-			</table>
-			<Link
-				className='text-white border px-4 rounded-md py-1 shadow hover:shadow-white/30 hover:shadow-md hover:bg-primary'
-				href={"./products/new"}>
-				Add new product
-			</Link>
+					</thead>
+					<tbody>
+						{products.map((product) => (
+							<tr
+								key={product._id}
+								className=''>
+								<td className='text-white pl-4'>{product.title}</td>
+								<td className='flex gap-1 border-l border-white/10 py-1'>
+									<Link
+										href={"/products/edit/" + product._id}
+										className='btn-accent'>
+										{/*bg-bgColor px-4 border hover:bg-white/10 hover:shadow-sm hover:shadow-white/10*/}
+										<svg
+											xmlns='http://www.w3.org/2000/svg'
+											fill='none'
+											viewBox='0 0 24 24'
+											strokeWidth={1.5}
+											stroke='currentColor'
+											className='w-4 h-4'>
+											<path
+												strokeLinecap='round'
+												strokeLinejoin='round'
+												d='M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10'
+											/>
+										</svg>
+										Edit
+									</Link>
+									<button
+										className=' btn-primary flex items-center shadow-sm '
+										onClick={() => deleteConfirmation(product, page, fetchProduct)}>
+										<svg
+											xmlns='http://www.w3.org/2000/svg'
+											fill='none'
+											viewBox='0 0 24 24'
+											strokeWidth={1.5}
+											stroke='currentColor'
+											className='w-4 h-4'>
+											<path
+												strokeLinecap='round'
+												strokeLinejoin='round'
+												d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
+											/>
+										</svg>
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+				<div className='py-3 mt-3'>
+					<Link
+						className='btn-layout'
+						href={"./products/new"}>
+						Add new product
+					</Link>
+				</div>
+			</div>
 		</Layout>
 	);
 }
