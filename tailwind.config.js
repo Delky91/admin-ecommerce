c85a1fb31ef05d940015891e6c83a8e560cdef34
Luis <l.mino.bustos@gmail.com>
2023-07-12 04:06:38 -0400

    new clean ui/ux design

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,13 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				primary: "#463172",
-				secundary: "#7C57CA",
-				mid: "#62459F",
-				txColor: "#ebefdc",
-				bgColor: "#494867",
-				bgFrom: "#F7CBFD",
-				bgTo: "#7778D1",
+				primary: "#f7d564",
+				secundary: "#020717",
+				bgColor: "#172554",
+				txColor: "#e8edfd",
+				accent: "#1541d1",
 			},
 			boxShadow: {
-				"t-sm": "0 -1px 2px 0 rgba(0, 0, 0, 0.05)",
-				"t-md":
-					"0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
-				"t-lg":
-					"0 -10px 15px -3px rgba(0, 0, 0, 0.1), 5px -5px 8px -2px rgba(0, 0, 0, 0.1)",
 				"c-hover": "0px 0px 4px 1px rgba(255,255,255, 0.3)",
 			},
 		},
