12a627490f34402e335ba15f9a0cffb3563a541f
Luis <l.mino.bustos@gmail.com>
2023-08-11 03:39:15 -0400

    added meta tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang='en'>
-			<Head />
+			<Head>
+			<meta title="author" content="Luis Miño Bustos" />
+			</Head>
 			<body className='scroll-smooth'>
 				<Main />
 				<NextScript />
